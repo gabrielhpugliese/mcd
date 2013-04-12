29998deefc7bebe905c3574d5af3d56c17ab93fd
gabrielhpugliese <gabrielhpugliese@gmail.com>
2013-04-12 11:34:20 -0300

    search numbers inicial

--- client/application.js
+++ client/application.js
@@ -5,6 +5,6 @@ Meteor.subscribe('Profiles', function () {
 });
 
 Meteor.startup(function () {
-    Session.set('search_numbers', [700,701]);
+    Session.set('search_numbers', [700,701,702,703,704,705,706,707,708,709,710,711,712,713,714,715,716,717,718,719,720,721,722,723]);
 });
 
